117157d8013f2fbec59488c976040f1f271fa519
Jakob Jordan <j.jordan@fz-juelich.de>
2017-11-02 14:59:03 +0100

    Add support for multidimensional search distributions

--- es/plain_es.py
+++ es/plain_es.py
@@ -5,9 +5,10 @@ from . import lib
 
 def optimize(func, learning_rate, mu, sigma,
              sigma_lower_bound=0.1, max_iter=2000, population_size=30,
-             tol=0.01, fitness_shaping=True, record_history=False):
+             fitness_shaping=True, record_history=False):
     """
     Evolutionary strategies using the plain gradient of multinormal search distributions.
+    Does not consider covariances between parameters.
     See Wierstra et al. (2014). Natural evolution strategies. Journal of Machine Learning Research, 15(1), 949-980.
     """
 
@@ -18,7 +19,7 @@ def optimize(func, learning_rate, mu, sigma,
     success = False
 
     while True:
-        z = np.random.normal(mu, sigma, size=population_size)
+        z = np.random.normal(mu, sigma, size=(population_size, *np.shape(mu)))
         fitness = np.array([func(zi) for zi in z])
 
         if fitness_shaping:
@@ -27,27 +28,25 @@ def optimize(func, learning_rate, mu, sigma,
         else:
             utility = fitness
 
+        mu_prev = mu.copy()
+
         # update parameter of search distribution via plain gradient descent
-        mu -= learning_rate * 1. / population_size * np.dot(z - mu, utility) * 1. / sigma ** 2
-        sigma -= learning_rate * 1. / population_size * np.dot((z - mu) ** 2 - sigma ** 2, utility) * 1. / sigma ** 3
+        mu -= learning_rate * 1. / population_size * np.dot(utility, z - mu) * 1. / sigma ** 2
+        sigma -= learning_rate * 1. / population_size * np.dot(utility, (z - mu) ** 2 - sigma ** 2) * 1. / sigma ** 3
 
         # enforce lower bound on sigma to avoid instabilities
-        if sigma <= sigma_lower_bound:
-            sigma = sigma_lower_bound
+        if np.any(sigma <= sigma_lower_bound):
+            sigma[sigma <= sigma_lower_bound] = sigma_lower_bound
 
         if record_history:
-            history_mu.append(mu)
-            history_sigma.append(sigma)
-            history_pop.append(z)
+            history_mu.append(mu.copy())
+            history_sigma.append(sigma.copy())
+            history_pop.append(z.copy())
 
         generation += 1
 
-        # exit if converged or max iterations reached
-        if len(history_mu) > 100 and np.std(history_mu[-100:]) < tol * np.abs(np.mean(history_mu[-100:])):
-            success = True
-            break
-        elif generation > max_iter:
-            success = False
+        # exit if max iterations reached
+        if generation > max_iter:
             break
 
-    return {'mu': mu, 'sigma': sigma, 'success': success, 'history_mu': history_mu, 'history_sigma': history_sigma, 'history_pop': history_pop}
+    return {'mu': mu, 'sigma': sigma, 'history_mu': history_mu, 'history_sigma': history_sigma, 'history_pop': history_pop}
--- test/test_es.py
+++ test/test_es.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
 import numpy as np
 import sys
 
@@ -6,17 +7,37 @@ sys.path.append('../es')
 
 from es import plain_es
 
+TOLERANCE_1D = 0.2
+TOLERANCE_2D = 0.2
+
 
 def test_quadratic_1d():
-    x0 = 5.
     eta = 0.01
-    mu = 1.
     sigma = 1.
 
-    def f(x):
-        return (x - x0) ** 2
+    for mu, x0 in zip(np.random.uniform(-5., 5., 10), np.random.uniform(-5., 5., 10)):
 
-    res = plain_es.optimize(f, eta, mu, sigma, record_history=True)
+        def f(x):
+            return (x - x0) ** 2
 
-    assert(res['success'])
-    assert(abs(res['mu'] - x0) < 1e-2)
+        res = plain_es.optimize(f, eta, np.array(mu), np.array(sigma), max_iter=3000)
+
+        assert(abs(res['mu'] - x0) < TOLERANCE_1D)
+
+
+def test_quadratic_2d():
+    eta = 0.01
+    sigma_x = 1.
+    sigma_y = 1.
+
+    for (mu_x, mu_y), (x0, y0) in zip(
+            zip(np.random.uniform(-5., 5., 10), np.random.uniform(-5., 5., 10)),
+            zip(np.random.uniform(-5., 5., 10), np.random.uniform(-5., 5., 10))):
+
+        def f(x):
+            return (x[0] - x0) ** 2 + (x[1] - y0) ** 2
+
+        res = plain_es.optimize(f, eta, np.array([mu_x, mu_y]), np.array([sigma_x, sigma_y]), max_iter=3000)
+
+        assert(abs(res['mu'][0] - x0) < TOLERANCE_2D)
+        assert(abs(res['mu'][1] - y0) < TOLERANCE_2D)
