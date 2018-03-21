206bd80d3cec518d038740805742291b5c637a21
Jakob Jordan <jakobjordan@posteo.de>
2018-03-21 17:59:27 +0100

    Add exponential natural evolution strategies
    based on first implementation by @mschmidt87

--- es/lib.py
+++ es/lib.py
@@ -22,6 +22,21 @@ def default_learning_rate_sigma(dimensions):
     return (3 + np.log(dimensions)) / (5. * np.sqrt(dimensions))
 
 
+def default_learning_rate_sigma_exponential(dimensions):
+    """
+    See Wierstra et al. (2014). Natural evolution strategies. Journal of Machine Learning Research, 15(1), 949-980.
+    """
+    return (9 + 3. * np.log(dimensions)) / (5. * dimensions * np.sqrt(dimensions))
+
+
+def default_learning_rate_B_exponential(dimensions):
+    """
+    Learning rate for B, seems to be much too large when using default value from Wierstra et al. (2014).
+    Hence reduce significantly.
+    """
+    return 0.005 * (9 + 3. * np.log(dimensions)) / (5. * dimensions * np.sqrt(dimensions))
+
+
 def utility(fitness):
     """
     Utility function for fitness shaping.
