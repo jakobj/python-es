12e74bf785be8b803377c4ef5e48cd7bc75acee3
Jakob Jordan <jakobjordan@posteo.de>
2018-05-29 13:57:55 +0200

    Use optimizer from torch to update mu in snes

--- es/separable_natural_es.py
+++ es/separable_natural_es.py
@@ -1,13 +1,17 @@
 import multiprocessing as mp
 import numpy as np
+import torch
+
 from . import lib
+
 
 def optimize(func, mu, sigma,
              learning_rate_mu=None, learning_rate_sigma=None, population_size=None,
              max_iter=2000,
              fitness_shaping=True, mirrored_sampling=True, record_history=False,
              rng=None,
-             parallel_threads=None):
+             parallel_threads=None,
+             optimizer=torch.optim.SGD):
     """
     Evolution strategies using the natural gradient of multinormal search distributions in natural coordinates.
     Does not consider covariances between parameters.
@@ -37,12 +41,21 @@ def optimize(func, mu, sigma,
     history_pop = []
     history_fitness = []
 
+    # convert mu to torch Variable and construct optimizer; force
+    # torch to use double representation
+    mu_torch = torch.autograd.Variable(torch.DoubleTensor(mu.copy()), requires_grad=True)
+    optimizer_mu = optimizer([mu_torch], lr=learning_rate_mu)
+
     while True:
-        s = rng.normal(0, 1, size=(population_size, *np.shape(mu)))
-        z = mu + sigma * s
+
+        # use numpy representation for generating individuals
+        mu_numpy = mu_torch.detach().numpy()
+
+        s = rng.normal(0, 1, size=(population_size, *np.shape(mu_numpy)))
+        z = mu_numpy + sigma * s
 
         if mirrored_sampling:
-            z = np.vstack([z, mu - sigma * s])
+            z = np.vstack([z, mu_numpy - sigma * s])
             s = np.vstack([s, -s])
 
         if parallel_threads is None:
@@ -65,26 +78,31 @@ def optimize(func, mu, sigma,
         else:
             utility = fitness
 
-        # update parameter of search distribution via natural gradient descent in natural coordinates
-        mu += learning_rate_mu * sigma * np.dot(utility, s)
-        sigma *= np.exp(learning_rate_sigma / 2. * np.dot(utility, s ** 2 - 1))
-
         if record_history:
-            history_mu.append(mu.copy())
+            history_mu.append(mu_numpy.copy())
             history_sigma.append(sigma.copy())
             history_pop.append(z.copy())
             history_fitness.append(fitness.copy())
-
-        generation += 1
 
         # exit if max iterations reached
         if generation > max_iter or np.all(sigma < 1e-10):
             break
 
-    return {'mu': mu,
+        # update parameters of search distribution via natural
+        # gradient descent in natural coordinates
+
+        # set gradient and use optimizer to update mu
+        mu_torch.grad = torch.autograd.Variable(torch.DoubleTensor(-sigma * np.dot(utility, s)))
+        optimizer_mu.step()
+
+        # manually update sigma
+        sigma *= np.exp(learning_rate_sigma / 2. * np.dot(utility, s ** 2 - 1))
+
+        generation += 1
+
+    return {'mu': mu_numpy,
             'sigma': sigma,
             'history_mu': history_mu,
             'history_sigma': history_sigma,
             'history_fitness': history_fitness,
             'history_pop': history_pop}
-  
